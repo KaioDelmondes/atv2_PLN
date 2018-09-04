9da8541b9a965a2defd420f51a087c2d4dda3961
KaioDelmondes <kaioregodearaujo@hotmail.com>
2018-09-04 15:25:01 -0300

    Cálculo da média feito. Resultado em float

--- questao2.py
+++ questao2.py
@@ -1,1 +1,10 @@
 from nltk.book import *
+
+def media():
+    fdist = FreqDist(len(w) for w in text1)
+    somaTotal = 0
+    for tam in fdist.most_common():
+        somaTotal += tam[0] * tam[1]   
+    
+    resultadoMedia = somaTotal/fdist.N()
+    return resultadoMedia
