ebbf63825a9da2f75ace3abf62307c776f160c70
Joshoa <joshoa6@hotmail.com>
2018-09-05 15:35:00 -0300

    Documentação melhorada

--- corpus_generator.py
+++ corpus_generator.py
@@ -2,14 +2,14 @@ import nltk
 from nltk import FreqDist
 from nltk.corpus import PlaintextCorpusReader 
 
-loc = input("Qual o caminho do data_corpus no seu pc? (/SEU_PATH/data_corpus/)")
-
 # O Corpus é criado através do PlaintextCorpusReader, os métodos fileids()
 # e words() são referentes ao corpus e suas aplicações.
 
-corpus = PlaintextCorpusReader(loc, '.*\.txt')
-corpus = PlaintextCorpusReader(loc, '.*\.txt')
+# para usar o corpus em outros arquivos basta copiar as linhas de codigo [10 - 13]
+# o `loc` é o path para o data_corpus no pc
 
+loc = input("Qual o caminho do data_corpus no seu pc? (/SEU_PATH/data_corpus/)")
+corpus = PlaintextCorpusReader(loc, '.*\.txt') 
 corpus.fileids()
 corpus.words()
 
